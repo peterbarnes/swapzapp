0c3bde4bab192f7eb56019fbd344534bbbda661a
Aaron Wright <acwrightdesign@gmail.com>
2014-03-27 22:41:54 -0500

    Fixes

--- app/assets/javascripts/pos/models/socket.js
+++ app/assets/javascripts/pos/models/socket.js
@@ -7,24 +7,24 @@ SwapzPOS.Socket = Ember.Object.extend({
     this.set('messages', Ember.A());
   },
   connect: function() {
-    try {
-      var account = this.get('account');
-      var _socket = io.connect(this.get('url') + '/' + account.get('namespace'));
-      var socket = this;
-      _socket.on('message', function(data) {
-        if (data.data) {
-          socket.get('messages').addObject(data.data);
-        }
-      });
-      this.set('socket', _socket);
-    } catch(error) {
-      console.log(error);
-    }
+    // try {
+    //   var account = this.get('account');
+    //   var _socket = io.connect(this.get('url') + '/' + account.get('namespace'));
+    //   var socket = this;
+    //   _socket.on('message', function(data) {
+    //     if (data.data) {
+    //       socket.get('messages').addObject(data.data);
+    //     }
+    //   });
+    //   this.set('socket', _socket);
+    // } catch(error) {
+    //   console.log(error);
+    // }
   }.observes('account'),
   send: function(data) {
-    var socket = this.get('socket');
-    if (socket) {
-      socket.emit('message', {data: data});
-    }
+    // var socket = this.get('socket');
+    // if (socket) {
+    //   socket.emit('message', {data: data});
+    // }
   }
 });
--- app/serializers/repair_serializer.rb
+++ app/serializers/repair_serializer.rb
@@ -1,4 +1,6 @@
 class RepairSerializer < ActiveModel::Serializer
+  delegate :params, to: :scope
+  
   attributes :id, :customer_id, :location_id, :store_id, :till_id, :user_id, :complete, :flagged, :identifier, :identifier_type, :note, :symptoms, :sku, :tax, :tax_rate, :completed_at, :created_at, :updated_at
   
   has_one :customer
@@ -10,4 +12,16 @@ class RepairSerializer < ActiveModel::Serializer
   has_one  :store, :serializer => StoreIndexSerializer
   has_one  :till, :serializer => TillIndexSerializer
   has_one  :user, :serializer => UserIndexSerializer
+  
+  def include_associations!
+    include! :customer if params[:customer]
+    include! :images
+    include! :lines
+    include! :location
+    include! :logs
+    include! :payment
+    include! :store
+    include! :till
+    include! :user
+  end
 end
